8d74b08d06009b41b4b7820f1e52000c8afecaaa
Anders Andreasen <58475535+andr1976@users.noreply.github.com>
2022-12-30 01:55:58 +0100

    First commit adding tcPR

--- ramdecom/wavespeed.py
+++ ramdecom/wavespeed.py
@@ -3,10 +3,129 @@
 import math
 import numpy as np
 import pandas as pd
+from thermopack import tcPR
 from matplotlib import pyplot as plt
 from cerberus import Validator
 from CoolProp.CoolProp import PropsSI
 import CoolProp.CoolProp as CP
+
+
+def tcpr_entropy(tc_pr, T, P, z):
+    x, y, beta_g, beta_l, phase = tc_pr.two_phase_tpflash(T, P, z)
+
+    if phase == tc_pr.TWOPH:
+        s_l, = tc_pr.entropy(T, P, x, tc_pr.LIQPH)
+        s_g, = tc_pr.entropy(T, P, y, tc_pr.VAPPH)
+        s = s_g * abs(beta_g) + s_l * abs(beta_l)
+    elif phase == tc_pr.LIQPH:
+        s_l, = tc_pr.entropy(T, P, z, tc_pr.LIQPH)
+        s = s_l
+    elif phase == tc_pr.VAPPH:
+        s_l, = tc_pr.entropy(T, P, z, tc_pr.VAPPH)
+        s = s_l
+    elif phase == tc_pr.SINGLEPH:
+        s_l, = tc_pr.entropy(T, P, z, tc_pr.VAPPH)
+        s = s_l
+    return s
+
+
+def tcpr_enthalpy(tc_pr, T, P, z):
+    x, y, beta_g, beta_l, phase = tc_pr.two_phase_tpflash(T, P, z)
+
+    if phase == tc_pr.TWOPH:
+        s_l, = tc_pr.enthalpy(T, P, x, tc_pr.LIQPH)
+        s_g, = tc_pr.enthalpy(T, P, y, tc_pr.VAPPH)
+        s = s_g * beta_g + s_l * beta_l
+    elif phase == tc_pr.LIQPH:
+        s_l, = tc_pr.enthalpy(T, P, z, tc_pr.LIQPH)
+        s = s_l
+    elif phase == tc_pr.VAPPH:
+        s_l, = tc_pr.enthalpy(T, P, z, tc_pr.VAPPH)
+        s = s_l
+    elif phase == tc_pr.SINGLEPH:
+        s_l, = tc_pr.enthalpy(T, P, z, tc_pr.VAPPH)
+        s = s_l
+    return s
+
+
+def tcpr_temperature(tc_pr, P, S, z, Tguess=None):
+    temp, x, y, beta_g, beta_l, phase = tc_pr.two_phase_psflash(P,
+                                                                z,
+                                                                S,
+                                                                temp=Tguess)
+
+    return temp
+
+
+def tcpr_speedofsound(tc_pr, T, P, z):
+    x, y, beta_g, beta_l, phase = tc_pr.two_phase_tpflash(T, P, z)
+
+    if phase == tc_pr.TWOPH:
+        sos = tc_pr.speed_of_sound(T,
+                                   P,
+                                   x=x,
+                                   y=y,
+                                   z=z,
+                                   betaV=beta_g,
+                                   betaL=beta_l,
+                                   phase=tc_pr.TWOPH)
+    elif phase == tc_pr.LIQPH:
+        sos = tc_pr.speed_of_sound(T,
+                                   P,
+                                   x=z,
+                                   y=z,
+                                   z=z,
+                                   betaV=0.0,
+                                   betaL=1.0,
+                                   phase=tc_pr.LIQPH)
+    elif phase == tc_pr.VAPPH:
+        sos = tc_pr.speed_of_sound(T,
+                                   P,
+                                   x=z,
+                                   y=z,
+                                   z=z,
+                                   betaV=1.0,
+                                   betaL=0.0,
+                                   phase=tc_pr.VAPPH)
+    elif phase == tc_pr.SINGLEPH:
+        sos = tc_pr.speed_of_sound(T,
+                                   P,
+                                   x=z,
+                                   y=z,
+                                   z=z,
+                                   betaV=0.0,
+                                   betaL=1.0,
+                                   phase=tc_pr.SINGLEPH)
+
+    return sos
+
+
+def tcpr_density(tc_pr, P, S, z, Tguess=None):
+    mw = np.zeros((tc_pr.nc))
+    for i in (range(len(mw))):
+        mw[i] = tc_pr.compmoleweight(i + 1) * 1.0e-3  # g/mol -> kg/mol
+
+    mw_z = np.matmul(z, mw)
+    print(mw_z)
+    T, x, y, beta_g, beta_l, phase = tc_pr.two_phase_psflash(P,
+                                                             z,
+                                                             S,
+                                                             temp=Tguess)
+    if phase == tc_pr.TWOPH:
+        v_l, = tc_pr.specific_volume(T, P, x, tc_pr.LIQPH)
+        v_g, = tc_pr.specific_volume(T, P, y, tc_pr.VAPPH)
+        v = v_g * abs(beta_g) + v_l * abs(beta_l)
+    elif phase == tc_pr.LIQPH:
+        v_l, = tc_pr.specific_volume(T, P, z, tc_pr.LIQPH)
+        v = v_l
+    elif phase == tc_pr.VAPPH:
+        v_l, = tc_pr.specific_volume(T, P, z, tc_pr.VAPPH)
+        v = v_l
+    elif phase == tc_pr.SINGLEPH:
+        v_l, = tc_pr.specific_volume(T, P, z, tc_pr.LIQPH)
+        v = v_l
+    rho = mw_z / v
+    return rho
 
 
 def validate_mandatory_ruleset(input):
@@ -28,11 +147,11 @@ def validate_mandatory_ruleset(input):
         'temperature': {
             'required': True,
             'type': 'number',
-            },
+        },
         'pressure': {
             'required': True,
             'type': 'number',
-        },                
+        },
         'extrapolate': {
             'required': False,
             'type': 'boolean',
@@ -48,7 +167,7 @@ def validate_mandatory_ruleset(input):
         'eos': {
             'required': True,
             'type': 'string',
-            'allowed': ['HEOS', 'REFPROP']
+            'allowed': ['HEOS', 'REFPROP', 'tcPR']
         },
         'fluid': {
             'required': True,
@@ -110,16 +229,16 @@ class WaveSpeed:
         if 'pressure_step' in self.input:
             self.P_step = self.input['pressure_step']
         else:
-            self.P_step = 1.0e5 
+            self.P_step = 1.0e5
         if 'pressure_break' in self.input:
             self.P_break = self.input['pressure_break']
         else:
-            self.P_break = 1.0e5 
+            self.P_break = 1.0e5
         if 'extrapolate' in self.input:
             self.extrapolate = self.input['extrapolate']
         else:
-            self.extrapolate = False 
-        
+            self.extrapolate = False
+
         self.T0 = self.input['temperature']
         self.P0 = self.input['pressure']
         self.eos = self.input['eos']
@@ -128,7 +247,10 @@ class WaveSpeed:
         self.max_step = int(self.P0 / self.P_step)
 
         if "&" in self.input["fluid"]:
-            comp_frac_pair = [str.replace("[", " ").replace("]", "").split(" ") for str in self.input["fluid"].split("&")]
+            comp_frac_pair = [
+                str.replace("[", " ").replace("]", "").split(" ")
+                for str in self.input["fluid"].split("&")
+            ]
             comp = [pair[0] for pair in comp_frac_pair]
             molefracs = np.asarray([float(pair[1]) for pair in comp_frac_pair])
             molefracs = molefracs / sum(molefracs)
@@ -136,9 +258,12 @@ class WaveSpeed:
             sep = "&"
             self.comp = sep.join(comp)
             self.single_component = False
-            
-            self.fluid = '&'.join([c + '[' + str(x) + ']'  for c,x in zip(self.comp.split('&'), self.molefracs)]) #self.input['fluid']
-            self.fluid_string = self.eos + '::' + self.fluid #self.input['fluid']
+
+            self.fluid = '&'.join([
+                c + '[' + str(x) + ']'
+                for c, x in zip(self.comp.split('&'), self.molefracs)
+            ])  #self.input['fluid']
+            self.fluid_string = self.eos + '::' + self.fluid  #self.input['fluid']
         # Normally single component fluid is specified
         else:
             self.comp = self.input["fluid"]
@@ -146,22 +271,27 @@ class WaveSpeed:
             self.fluid = self.input['fluid']
             self.fluid_string = self.eos + '::' + self.input['fluid']
 
+        if self.eos == 'tcPR':
+            self.tcpr = tcPR.tcPR()
+            self.tcpr.init(self.comp.replace('&', ','))
+            self.z = self.molefracs
+
         if self.eos == 'REFPROP' and 'refprop_option' in self.input:
             if self.input['refprop_option'] == 'GERG':
                 CP.set_config_bool(CP.REFPROP_USE_GERG, True)
             elif self.input['refprop_option'] == 'PR':
                 CP.set_config_bool(CP.REFPROP_USE_PENGROBINSON, True)
 
-
     def initialize(self):
         """
-        Setting inital entropy for the isentrope, and preparing lists 
+        Setting initial entropy for the isentrope, and preparing lists 
         for storing results. 
         """
-        self.S0 = PropsSI('Smass', 'P', self.P0, 'T', self.T0, self.fluid_string)
-        self.asfluid = CP.AbstractState(self.eos, self.comp)
-        self.asfluid.set_mole_fractions(self.molefracs)
-        self.asfluid.update(CP.PT_INPUTS, self.P0, self.T0)
+        self.S0 = self.entropy(P=self.P0, T=self.T0)
+        if self.eos == 'HEOS' or self.eos == 'REFPROP':
+            self.asfluid = CP.AbstractState(self.eos, self.comp)
+            self.asfluid.set_mole_fractions(self.molefracs)
+            self.asfluid.update(CP.PT_INPUTS, self.P0, self.T0)
         self.T = []
         self.P = []
         self.S_mass = []
@@ -170,10 +300,46 @@ class WaveSpeed:
         self.Q = []
         self.C = []
         self.U = []
-        self.W = []        
+        self.W = []
+        self.Tguess = None
 
+    def density(self, P):
+        if self.eos == 'HEOS' or self.eos == 'REFPROP':
+            rho = PropsSI('Dmass', 'S', self.S0, 'P', P, self.fluid_string)
+        elif self.eos == 'tcPR':
+            rho = tcpr_density(self.tcpr,
+                               P,
+                               self.S0,
+                               self.z,
+                               Tguess=self.Tguess - 1)
+        return rho
 
-    def speed_of_sound(self, Smass, P1):
+    def entropy(self, P, T):
+        if self.eos == 'HEOS' or self.eos == 'REFPROP':
+            S = PropsSI('Smass', 'P', P, 'T', T, self.fluid_string)
+        elif self.eos == 'tcPR':
+            S = tcpr_entropy(self.tcpr, T, P, self.z)
+        return S
+
+    def enthalpy(self, P, T):
+        if self.eos == 'HEOS' or self.eos == 'REFPROP':
+            H = PropsSI('Hmass', 'P', P, 'T', T, self.fluid_string)
+        elif self.eos == 'tcPR':
+            H = tcpr_enthalpy(self.tcpr, T, P, self.z)
+        return H
+
+    def temperature_PS(self, P):
+        if self.eos == 'HEOS' or self.eos == 'REFPROP':
+            T = PropsSI('T', 'P', P, 'S', self.S0, self.fluid_string)
+        elif self.eos == 'tcPR':
+            T = tcpr_temperature(self.tcpr,
+                                 P,
+                                 self.S0,
+                                 self.z,
+                                 Tguess=self.Tguess - 1)
+        return T
+
+    def speed_of_sound(self, P1):
         """
         Generic calculation of the fluid speed of sound using 
         a finite difference approximation to the expression
@@ -199,41 +365,62 @@ class WaveSpeed:
         retval : float 
             Speed of sound    
         """
-        
-        rho1 = PropsSI('Dmass', 'S', Smass, 'P', P1, self.fluid_string)
-        P2=P1+self.del_P
-        rho2 = PropsSI('Dmass', 'S', Smass, 'P', P2, self.fluid_string)
+
+        if self.eos == 'tcPR':
+            T = self.temperature_PS(P1)
+            x, y, beta_g, beta_l, phase = self.tcpr.two_phase_tpflash(
+                T, P1, self.z)
+            if phase == self.tcpr.TWOPH:
+                try:
+                    retval = tcpr_speedofsound(self.tcpr, T, P1, self.z)
+                except:
+                    print(self.Tguess)
+                    raise
+                return retval
+            #T = self.asfluid.T()
+        #P = self.asfluid.P()
+        #self.asfluid.update(CP.PSmass_INPUTS, P1, Smass)
+        #rho1 = self.asfluid.rhomass()
+        Smass = self.S0
+        rho1 = self.density(P1)
+        P2 = P1 + self.del_P
+        #self.asfluid.update(CP.PSmass_INPUTS, P2, Smass)
+        #rho2 = self.asfluid.rhomass()
+        rho2 = self.density(P2)
+
         try:
-            retval = math.sqrt((P2-P1)/(rho2-rho1))
+            retval = math.sqrt((P2 - P1) / (rho2 - rho1))
         except:
-            print("P:", P1, "T:", PropsSI('T', 'S', Smass, 'P', P1, self.fluid_string))
-            raise 
+            print("P:", P1, "T:",
+                  PropsSI('T', 'S', Smass, 'P', P1, self.fluid_string))
+            raise
 
         return retval
 
     def get_dataframe(self):
         if self.isrun == True:
-            data = {'Pressure (Pa)': self.P, 
-                    'Temperature (K)': self.T,
-                    'Speed of sound (m/s)': self.C,
-                    'Bernouilli velocity (m/s)': self.U,
-                    'Decompression wave speed (m/s)': self.W,
-                    'Fluid density (kg/m3)': self.rho_mass,
-                    'Fluid enthalpy (J/kg)': self.H_mass
-                    }
-            df=pd.DataFrame(data)
+            data = {
+                'Pressure (Pa)': self.P,
+                'Temperature (K)': self.T,
+                'Speed of sound (m/s)': self.C,
+                'Bernouilli velocity (m/s)': self.U,
+                'Decompression wave speed (m/s)': self.W,
+                'Fluid density (kg/m3)': self.rho_mass,
+                'Fluid enthalpy (J/kg)': self.H_mass
+            }
+            df = pd.DataFrame(data)
             self.df = df
             return df
-        else: 
-            return None    
-    
+        else:
+            return None
+
     def get_result_file(self, filename=None):
         if self.isrun:
             if filename:
                 self.get_dataframe().to_csv(filename)
             else:
                 self.get_dataframe().to_csv('decom_result.csv')
-        
+
     def plot_envelope(self, t_min=250, filename=None):
         """
         Convenience function to provide easy plotting of the 
@@ -253,10 +440,22 @@ class WaveSpeed:
             ps = CP.PropsSI('P', 'T', Ts, 'Q', 0, self.fluid_string)
 
             plt.plot(Ts, ps, color='dimgrey', label='Saturation line')
-            plt.plot(self.T, self.P,'k--', label='Isentropic path')
+            plt.plot(self.T, self.P, 'k--', label='Isentropic path')
             plt.plot(Tc, pc, 'ko', label='Critical point')
-            plt.plot(Tt, pt, linestyle='none', marker='o', color='black', fillstyle='none', label='Triple point')
-            plt.plot(self.T0, self.P0, linestyle='none', marker='o', color='k', fillstyle='right', label='Initial state')
+            plt.plot(Tt,
+                     pt,
+                     linestyle='none',
+                     marker='o',
+                     color='black',
+                     fillstyle='none',
+                     label='Triple point')
+            plt.plot(self.T0,
+                     self.P0,
+                     linestyle='none',
+                     marker='o',
+                     color='k',
+                     fillstyle='right',
+                     label='Initial state')
         else:
             self.asfluid.build_phase_envelope("")
             PE = self.asfluid.get_phase_envelope_data()
@@ -269,30 +468,30 @@ class WaveSpeed:
         plt.ylabel('Pressure (Pa)')
         plt.legend(loc='best')
         if filename:
-            plt.savefig(filename,dpi=600)
+            plt.savefig(filename, dpi=600)
         else:
             plt.show()
         plt.clf()
 
-    def plot_decom(self,data=None, filename=None):
+    def plot_decom(self, data=None, filename=None):
         """
         Convenience function to provide easy plotting of the 
         pressure vs decompression wave speed. 
         """
         plt.plot(self.W, self.P, 'k--', label="Calculated")
         if type(data) != type(None):
-            plt.plot(data[:,0], data[:,1]*1e5, 'ko', label="Experimental")
-            
+            plt.plot(data[:, 0], data[:, 1] * 1e5, 'ko', label="Experimental")
+
         plt.legend(loc='best')
         plt.xlabel("Decompression wave speed (m/s)")
         plt.ylabel("Pressure (Pa)")
         if filename:
-            plt.savefig(filename,dpi=600)
+            plt.savefig(filename, dpi=600)
         else:
             plt.show()
         plt.clf()
 
-    def run(self,disable_pbar=False):
+    def run(self, disable_pbar=False):
         """
         Main function to run through the isentropic path from initial P,T
         and stepping down in P along the isentrope. For each pressure step
@@ -305,36 +504,38 @@ class WaveSpeed:
                 Tguess = self.T0
             else:
                 Tguess = self.T[-1]
+            self.Tguess = Tguess
+            P_new = self.P0 - self.P_step * i
+            #self.asfluid.update(CP.PSmass_INPUTS, P_new, self.S0)
+            #Q = PropsSI('Q', 'P', P_new, 'S', self.S0, self.fluid_string)
+            Q = 0
+            #Q = self.asfluid.Q()
 
-            P_new = self.P0-self.P_step*i
-            T_new = PropsSI('T', 'P', P_new, 'S', self.S0, self.fluid_string)
-            #S_mass = PropsSI('Smass', 'P', P_new, 'T', T_new, self.fluid_string)
-            H_mass = PropsSI('Hmass', 'P', P_new, 'S', self.S0, self.fluid_string)
-            Q = PropsSI('Q', 'P', P_new, 'S', self.S0, self.fluid_string)
-            
             if Q < 0:
                 Q = 0
             elif Q > 1:
                 Q = 1
-
-            D_mass = PropsSI('Dmass', 'P', P_new, 'S', self.S0, self.fluid_string)
-            
             try:
-                C = self.speed_of_sound(self.S0, P_new)
+                T_new = self.temperature_PS(P_new)
+                H_mass = self.enthalpy(P_new, T_new)
+                D_mass = self.density(P_new)
+                print("Density", D_mass)
+
+                C = self.speed_of_sound(P_new)
             except:
                 self.isrun = True
                 break
-            
+
             if i == 0:
                 U = (self.P0 - P_new) / (C * D_mass)
             else:
-                U = self.U[i-1] + (self.P[i-1] - P_new) / (C * D_mass)
+                U = self.U[i - 1] + (self.P[i - 1] - P_new) / (C * D_mass)
 
             W = C - U
 
             if W > 0 and P_new > self.P_break:
                 self.C.append(C)
-                self.P.append(P_new)    
+                self.P.append(P_new)
                 self.T.append(T_new)
                 self.Q.append(Q)
                 self.H_mass.append(H_mass)
@@ -350,23 +551,25 @@ class WaveSpeed:
                 else:
                     self.isrun = True
                     break
-            
+            #self.asfluid.update(CP.PT_INPUTS, P_new, T_new)
             self.isrun = True
-            
+
+
 if __name__ == '__main__':
     input = {}
-    input['temperature'] = 273.15+32.
-    input['pressure'] = 120.4e5
-    input['eos'] = 'HEOS'
-    input['fluid'] = 'CO2'
-    #input['refprop_option']='GERG'
+    input['temperature'] = 273.15 + 35.2
+    input['pressure'] = 149.3e5
+    input['eos'] = 'REFPROP'
+    input['fluid'] = 'CO2[0.9677]&H2[0.0323]'
+    input['extrapolate'] = True
+    input['refprop_option'] = 'GERG'
     ws = WaveSpeed(input)
     ws.run()
 
-    data=np.loadtxt(r"..\validation\Botros_Test_15.txt",delimiter='\t')
-    
+    data = np.loadtxt(r"..\validation\Botros_Test_10B.txt", delimiter='\t')
+
     plt.plot(ws.W, ws.P, 'k--', label="Calculated")
-    plt.plot(data[:,0], data[:,1]*1e5, 'ko', label="Experimental")
+    plt.plot(data[:, 0], data[:, 1] * 1e5, 'ko', label="Experimental")
     plt.legend(loc='best')
     plt.xlabel("Decompression wave speed (m/s)")
     plt.ylabel("Pressure (Pa)")
--- scripts/validate_mixture.py
+++ scripts/validate_mixture.py
@@ -5,48 +5,52 @@ from matplotlib import pyplot as plt
 from CoolProp import CoolProp as CP
 import pandas as pd
 import numpy as np
-import matplotlib 
+import matplotlib
 
 matplotlib.rcParams.update({'font.size': 16})
-matplotlib.rc('legend', fontsize=14) 
+matplotlib.rc('legend', fontsize=14)
 matplotlib.rc('lines', linewidth=2.0)
 matplotlib.rc('axes', linewidth=2.0)
 
 exp = pd.read_csv(r'..\validation\mixture.csv', sep=';')
-input ={}
-input['eos'] = 'REFPROP' 
-#input['refprop_option'] = 'PR'
-color = ['red','blue','black','green','cyan','grey'] + ['red','blue','black','green','cyan','grey'] 
-
+input = {}
+input['eos'] = 'REFPROP'
+#input['refprop_option'] = 'GERG'
+color = ['red', 'blue', 'black', 'green', 'cyan', 'grey'
+         ] + ['red', 'blue', 'black', 'green', 'cyan', 'grey']
 
 for index, row in exp.iterrows():
-    if True:
+    if True and index == 3:
         # 1,2,4,5,
         # plt.figure(index)
         input['pressure'] = row['P (bar)'] * 1e5
-        input['temperature'] = row['T (C)'] + 273.15 
+        input['temperature'] = row['T (C)'] + 273.15
         input['fluid'] = row['Fluid']
-        input['pressure_step'] = 1e5
+        input['pressure_step'] = 0.5e5
         input['pressure_break'] = 1e5
         if index == 0:
             input['pressure_break'] = 70e5
-        ws = wavespeed.WaveSpeed(input)    
+        ws = wavespeed.WaveSpeed(input)
         ws.run()
-        filename = "..\\validation\\"  + row['File']
+        filename = "..\\validation\\" + row['File']
         data = np.loadtxt(filename, delimiter='\t')
-        plt.plot(ws.W,ws.P,color = color[index])#, label='Calculated')
-        plt.plot(data[:,0],data[:,1]*1e5,'o', color = color[index], label=row['Source']+row['Exp. No. '])
+        plt.plot(ws.W, ws.P, color=color[index])  #, label='Calculated')
+        plt.plot(data[:, 0],
+                 data[:, 1] * 1e5,
+                 'o',
+                 color=color[index],
+                 label=row['Source'] + row['Exp. No. '])
 
 plt.xlabel("Decompression wavespeed (m/s)")
 plt.ylabel("Presseure (Pa)")
 plt.legend(loc='best')
-plt.savefig("mix_combined.png",dpi=600,bbox_inches='tight')
-        #plt.clf()
+plt.savefig("mix_combined_new.png", dpi=600, bbox_inches='tight')
+#plt.clf()
 #plt.figure(2)
 # for index, row in exp.iterrows():
 #     input['pressure'] = row['P (bar)'] * 1e5
 #     input['temperature'] = row['T (C)'] + 273.15
-#     ws = wavespeed.WaveSpeed(input)    
+#     ws = wavespeed.WaveSpeed(input)
 #     ws.run()
 #     plt.plot(ws.T,ws.P,color = color[index], label=row['Source']+row['Exp No.'])
 
